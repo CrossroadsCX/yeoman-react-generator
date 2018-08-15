79164a0c00295e19b0b722f59b77f3b0508168e1
Chris Birk <cmbirk@gmail.com>
2018-08-14 20:57:56 -0500

    add eslint config, chalk dependency

--- generators/index.js
+++ generators/index.js
@@ -1,3 +1,17 @@
 const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 
-module.exports = class extends Generator {};
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  async prompting() {
+    const answers = await this.prompt([{
+      type: 'input',
+      name: 'name',
+      message: 'Project name',
+      default: this.appname, // Default to current folder name
+    }]);
+  }
+};
