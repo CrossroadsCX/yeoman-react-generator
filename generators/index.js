8bb296d6f6f1915d155714414ed88d5dabb58adf
Chris Birk <cmbirk@gmail.com>
2018-08-14 21:39:26 -0500

    add lint script, yarn installation

--- generators/index.js
+++ generators/index.js
@@ -1,17 +1,24 @@
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
+// const chalk = require('chalk');
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
-  }
+  // (constructor(args, opts) {
+  //     super(args, opts);
+  //   })
 
   async prompting() {
-    const answers = await this.prompt([{
+    this.answers = await this.prompt([{
       type: 'input',
       name: 'name',
       message: 'Project name',
       default: this.appname, // Default to current folder name
     }]);
+  }
+
+  async install() {
+    this.yarnInstall([
+      'react',
+      'redux',
+    ]);
   }
 };
