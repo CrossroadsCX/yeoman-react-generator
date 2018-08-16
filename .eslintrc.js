479aff86ba01d7465dac4aac35bba324793b3cee
Chris Birk <cmbirk@gmail.com>
2018-08-16 16:29:58 -0500

    change import errors to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+    extends: "airbnb-base",
+    rules: {
+      'import/no-unresolved': 1,
+      'import/no-extraneous-dependencies': 1,
+    },
 };
