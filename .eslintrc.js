8a666aa58ecb3d2b29d576f389f7ea6e8a72bade
Chris Birk <cmbirk@gmail.com>
2018-08-16 16:49:15 -0500

    update eslint to include react plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,3 @@
 module.exports = {
-    extends: "airbnb-base",
-    rules: {
-      'import/no-extraneous-dependencies': [
-        "warn", {"devDependencies": true}
-      ],
-    },
+    "extends": "airbnb"
 };
